d1554552bdaecebbf8289cb14feb33f310d2146e
Agustin <belcebutesla@gmail.com>
2025-07-28 11:56:14 -0300

    fix: repaired class Mapper

--- src/main/java/com/github/Ignacio/my_Library_In_Spring/DTOs/Mapper.java
+++ src/main/java/com/github/Ignacio/my_Library_In_Spring/DTOs/Mapper.java
@@ -4,7 +4,24 @@ import com.github.Ignacio.my_Library_In_Spring.Entity.Book;
 
 public class Mapper {
 
-    public Book toEntity(BookRequest request){
-        return new Book(request.getId(),request.getAuthor());
+    public Book toEntityBook(BookRequest request){
+        return new Book(request.getId()
+                ,request.getTitle()
+                ,request.getAuthor()
+                ,request.getEditorial()
+                ,request.getGender()
+                ,true
+                ,request.getYearOfPublication()
+        );
     }
+
+    public BookResponse toBookResponse(Book request){
+        return new BookResponse(request.getGender()
+                ,request.getAuthor()
+                ,request.getTitle()
+                ,request.getId()
+        );
+    }
+
+
 }
